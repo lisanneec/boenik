45009625273755d9b0c91403d2c83a8373d6dd85
lisanneec <laisuncroese@gmail.com>
2021-03-15 08:56:37 +0100

    tabel

--- ConcertInformatieSQL.py
+++ ConcertInformatieSQL.py
@@ -10,7 +10,11 @@ import sqlite3
 with sqlite3.connect("database.db") as db:
     cursor = db.cursor()
 ### ---------Functie definities  ----------------------
-
-
+def MaakNieuweTabellen():
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTIS tabel_artiest_gegevens(
+        artiest_nummer_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+        artiest_naam TEXT NOT NULL,
+        artiest_band TEXT NOT NULL);""")
 
 ### ---------Hoofdprogramma  ----------------
