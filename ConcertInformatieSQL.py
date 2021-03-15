50954b59c65ee2e69fc0323939456b5b969e0f78
lisanneec <laisuncroese@gmail.com>
2021-03-15 08:39:56 +0100

    namen

--- ConcertInformatieSQL.py
+++ ConcertInformatieSQL.py
@@ -1,8 +1,8 @@
 # Vul hier de naam van je programma in:
-# 
+# Concert informatie 
 # 
 # Vul hier jullie namen in:
-# 
+# Bo en Lisanne
 # 
 
 ### ---------Bibliotheken en globale variabelen -------
